ae159d880857cb1d2bb2e71b92ead329957288b3
PhaNith <nochphanith@gmail.com>
2022-06-17 17:01:40 +0700

    added pages

---
+++ src/app/budgetlist/budgetlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budgetlist',
+  templateUrl: './budgetlist.component.html',
+  styleUrls: ['./budgetlist.component.css']
+})
+export class BudgetlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
